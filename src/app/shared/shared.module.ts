67a7ac450fbc31657bdb2c337e93bf4301c1f386
Ihor Murzich <murzichigor@outlook.com>
2018-05-21 04:00:09 +0300

    infiniteScroll blank

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { InfiniteScrollDirective } from './directives/infiniteScroll/infiniteScroll.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
+    InfiniteScrollDirective,
     HeaderComponent,
     FooterComponent
   ],
@@ -13,6 +15,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule
   ],
   exports: [
+    InfiniteScrollDirective,
     HeaderComponent,
     FooterComponent
   ],
