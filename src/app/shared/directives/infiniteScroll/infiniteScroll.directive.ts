8615f465e98afdbb15a075324ae70c93e300b402
Ihor Murzich <murzichigor@outlook.com>
2018-05-23 06:08:01 +0300

    prepared InfiniteScroll Directive file with working eventListeners of scroll & resize

--- src/app/shared/directives/infiniteScroll/infiniteScroll.directive.ts
+++ src/app/shared/directives/infiniteScroll/infiniteScroll.directive.ts
@@ -1,6 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appInfiniteScroll]',
+  selector: '[appInfiniteScroll]'
 })
-export class InfiniteScrollDirective { }
+export class InfiniteScrollDirective {
+  private crossbarDelta: number;
+  private crossbar: number;
+  constructor() {
+    console.log('infiniteScroll Class created.');
+    this.crossbarDelta = 150;
+    this.crossbar = document.body.offsetHeight - window.innerHeight - this.crossbarDelta;
+  }
+  @HostListener('window:resize')
+  public onResize() {
+    this.crossbar = document.body.offsetHeight - window.innerHeight - this.crossbarDelta;
+  }
+  @HostListener('window:scroll')
+  public onScroll() {
+    if (window.scrollY > this.crossbar) {
+      console.log(window.scrollY);
+    }
+  }
+}
