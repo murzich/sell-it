ef51287173af3860e992c4f613162323cd4641fd
Ihor Murzich <murzichigor@outlook.com>
2018-05-26 15:51:15 +0300

    added displayed image for products from API without ones

--- src/app/productPage/productItem/productItem.component.ts
+++ src/app/productPage/productItem/productItem.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './productItem.component.html',
   styleUrls: ['./productItem.component.scss']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
   @Input() id: number;
   @Input() name: string;
   @Input() image: string;
   constructor() { }
+
+  ngOnInit() {
+    this.image = !this.image ? '/assets/img/no_image_available.jpg' : this.image;
+  }
 }
