d9338193a7fb4611da6a8117423317727c7bdad2
Ihor Murzich <murzichigor@outlook.com>
2018-06-07 08:00:57 +0300

    renamed directories to kebab-case

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { AdvertResolverService } from './core/advert-resolver.service';
 
 const routes: Routes = [
-  {path: 'login', loadChildren: './loginPage/loginPage.module#LoginPageModule'},
-  {path: 'product', loadChildren: './productPage/productPage.module#ProductPageModule'},
-  {path: 'detail/:id', loadChildren: './detailPage/detailPage.module#DetailPageModule', resolve: {advert: AdvertResolverService}},
+  {path: 'login', loadChildren: './login-page/loginPage.module#LoginPageModule'},
+  {path: 'product', loadChildren: './product-page/productPage.module#ProductPageModule'},
+  {path: 'detail/:id', loadChildren: './detail-page/detailPage.module#DetailPageModule', resolve: {advert: AdvertResolverService}},
   {path: 'new-advert', loadChildren: './add-advert/add-advert.module#AddAdvertModule'},
   {path: '', redirectTo: 'product', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
