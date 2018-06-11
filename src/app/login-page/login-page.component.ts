619e904a02a49d52909331c18682a2d1d8705ee5
murzich <murzichigor@outlook.com>
2018-06-11 19:22:19 +0300

    mock key on login page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent { }
+export class LoginPageComponent implements OnInit {
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.queryParams.subscribe(
+      value => console.log(value)
+    );
+  }
+
+
+}
