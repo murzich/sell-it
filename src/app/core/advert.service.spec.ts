8bdd2ed2b1ed11810f89e57bdc04fc8d68358aff
Ihor Murzich <murzichigor@outlook.com>
2018-07-16 02:39:19 +0300

    AdvertService.spec - Added network error testing, modified mockData in getAdverts() testing.

--- src/app/core/advert.service.spec.ts
+++ src/app/core/advert.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpRequest } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
 
@@ -28,29 +28,73 @@ describe('AdvertService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should return the json', () => {
-    const testingUrl = ApiUrls.adverts;
-    const mockJson = {
-      owner: {},
-      price: 15,
-      text: 'mock-text',
-      theme: '',
-      id: 32,
-    };
-    const responseJson = new AdvertFull(mockJson);
+  describe('.getAdverts()', () => {
 
-    service.getAdverts().subscribe(data => {
-      expect(data[0]).toEqual(responseJson);
+  const checkUrl = (url) => (request: HttpRequest<any>): boolean => {
+    return url.includes(request.url);
+  };
+  const testingUrl = ApiUrls.adverts;
+  const mockJson = { results: [{
+    owner: {},
+    price: 15,
+    text: 'mock-text',
+    theme: '',
+    id: 32,
+  }]};
+  const resultData = mockJson.results.map(advert => new AdvertFull(advert));
+
+
+    it('should return the AdvertFull[]', () => {
+
+      service.getAdverts().subscribe(data => {
+        expect(data).toEqual(resultData);
+      });
+
+      const req = httpMock.expectOne(checkUrl(testingUrl), 'call to api');
+      expect(req.request.method).toBe('GET');
+
+      req.flush(mockJson);
+
+      httpMock.verify();
     });
 
-    const checkUrl = (url) => (request: HttpRequest<any>): boolean => {
-      return url.includes(request.url);
-    };
-    const req = httpMock.expectOne(checkUrl(testingUrl), 'call to api');
-    expect(req.request.method).toBe('GET');
+    it('can test for 404 error', () => {
+      const emsg = 'deliberate 404 error';
 
-    req.flush({
-      results: [mockJson]
+      service.getAdverts().subscribe(
+        data => fail('should have failed with the 404 error'),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toEqual(404, 'status');
+          expect(error.error).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(checkUrl(testingUrl));
+
+      // Respond with mock error
+      req.flush(emsg, { status: 404, statusText: 'Not Found' });
+    });
+
+    it('can test for network error', () => {
+      const emsg = 'simulated network error';
+
+      service.getAdverts().subscribe(
+        data => fail('should have failed with the network error'),
+        (error: HttpErrorResponse) => {
+          expect(error.error.message).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(checkUrl(testingUrl));
+
+      // Create mock ErrorEvent, raised when something goes wrong at the network level.
+      // Connection timeout, DNS error, offline, etc
+      const mockError = new ErrorEvent('Network error', {
+        message: emsg,
+      });
+
+      // Respond with mock error
+      req.error(mockError);
     });
   });
 });
