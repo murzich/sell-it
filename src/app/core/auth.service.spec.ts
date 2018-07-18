6469f1bad7a7f054ca6ef94e6c1c501523494db6
Ihor Murzich <murzichigor@outlook.com>
2018-07-18 22:37:25 +0300

    AuthService.spec - .register() tests.

--- src/app/core/auth.service.spec.ts
+++ src/app/core/auth.service.spec.ts
@@ -323,4 +323,118 @@ describe('AuthService', () => {
       expect(router.navigate).not.toHaveBeenCalled();
     });
   });
+
+  describe('.register POST method', () => {
+    const url = ApiUrls.register;
+    const formData = {
+      email: 'user@email.com',
+      passwordGroup: {
+        password: '123456',
+        passwordConfirm: '123456',
+      },
+    };
+    const expectedResponse = {
+      user: {
+        email: 'user@email.com',
+        id: 1,
+        avatar: null,
+        username: 'User'
+      },
+      token: 'test-token',
+    };
+    const handledErrorMsg = 'handled error';
+
+    beforeEach(() => {
+      spyOn(service, 'handleError').and.returnValue(throwError(handledErrorMsg));
+    });
+
+    it('should return userData and token', () => {
+      const preparedData = {
+        email: formData.email,
+        password1: formData.passwordGroup.password,
+        password2: formData.passwordGroup.password,
+        username: undefined,
+      };
+
+      service.register(formData).subscribe(
+        (response) => expect(response).toEqual(expectedResponse)
+      );
+
+      const req = httpMock.expectOne(url, 'urls is not as expected');
+      expect(req.request.method).toBe('POST', 'http mehod wasn\'t POST');
+      expect(({...req.request.body})).toEqual(preparedData, 'backend gets unexpected data');
+
+      req.flush(expectedResponse);
+    });
+
+    it('should call sessionService', () => {
+      service.register(formData).subscribe(
+        (response) => {
+          expect(session.setSession.calls.count())
+            .toBe(1, 'wasn\'t called once');
+          expect(session.setSession.calls.mostRecent().args[0])
+            .toEqual(expectedResponse, 'response data was changed');
+
+          expect(service.handleError.calls.count())
+            .toBe(0, 'Error handler fn was called');
+        }
+      );
+
+      const req = httpMock.expectOne(url, 'urls is not as expected');
+
+      req.flush(expectedResponse);
+    });
+
+    it('should handle errors', () => {
+      const emsg = 'deliberate 400 error';
+
+      service.register(formData).subscribe(
+        data => fail('should have failed with the 400 error'),
+        (error) => {
+          // The errors are handling before they will be thrown outside.
+          expect(error.status).not.toEqual(400, 'status');
+          expect(error.error).not.toEqual(emsg, 'message');
+
+          expect(session.setSession.calls.count())
+            .not.toBeGreaterThan(0, 'setSession shouldn\'t be called');
+          expect(service.handleError.calls.count())
+            .toBe(1, 'Error handler fn wasn\'t called');
+
+          expect(error).toBe(handledErrorMsg);
+        }
+      );
+
+      const req = httpMock.expectOne(url, 'urls is not as expected');
+
+      req.flush(emsg, {status: 400, statusText: 'Bad request'});
+
+      httpMock.verify();
+    });
+
+    it('can test for network error', () => {
+      const emsg = 'simulated network error';
+
+      service.register(formData).subscribe(
+        data => fail('should have failed with the network error'),
+        (error) => {
+          expect(error.message).not.toEqual(emsg, 'message');
+
+          expect(session.setSession.calls.count())
+            .not.toBeGreaterThan(0, 'setSession shouldn\'t be called');
+          expect(service.handleError.calls.count())
+            .toBe(1, 'Error handler fn wasn\'t called');
+
+          expect(error).toBe(handledErrorMsg);
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+
+      const mockError = new ErrorEvent('Network error', {
+        message: emsg,
+      });
+
+      req.error(mockError);
+    });
+  });
 });
