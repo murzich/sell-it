82078d2e12892284b0ad7bee0886ec35dcbf3338
Ihor Murzich <murzichigor@outlook.com>
2018-07-20 07:22:30 +0300

    ProfileService.spec - put/patchProfile$ & get/set sessionProfile tests.

--- src/app/core/profile.service.spec.ts
+++ src/app/core/profile.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
@@ -115,7 +116,37 @@ describe('Profile Service', () => {
       req.flush(userProfileJson);
     });
 
-    // TODO: Add error checking
+    it('can test for 403 error', () => {
+      const emsg = 'deliberate 403 error';
+
+      service.getFreshProfile$().subscribe(
+        data => fail('should have failed with the 403 error'),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toEqual(403, 'status');
+          expect(error.error).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.flush(emsg, { status: 403, statusText: 'Forbidden' });
+    });
+
+    it('can test for network error', () => {
+      const emsg = 'simulated network error';
+      const mockError = new ErrorEvent('Network error', {
+        message: emsg,
+      });
+      service.getFreshProfile$().subscribe(
+        data => fail('should have failed with the network error'),
+        (error: HttpErrorResponse) => {
+          expect(error.error.message).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.error(mockError);
+    });
+
     it('should cast observable with current UserProfile', () => {
       const spyCondition = spyOn(service, 'hasTokenWithoutProfile')
         .and.returnValue(true);
@@ -131,6 +162,152 @@ describe('Profile Service', () => {
       // spyWorker gets it's internal data from backend.
       const req = httpMock.expectOne(url);
       req.flush(userProfileJson);
+    });
+  });
+
+  describe('.patchProfile$', () => {
+    const userProfileDelta = {username: 'patchedUser'} as UserProfile;
+    const userProfileResponse = {
+      ...userProfileJson,
+      ...userProfileDelta
+    } as UserProfile;
+
+    it('should produce PATCH http request', () => {
+      service.patchProfile$(userProfileDelta)
+        .subscribe((res) => {
+          expect(res.username).toBe(userProfileDelta.username);
+          expect(userProfileSetSpy).toHaveBeenCalled();
+          expect(userProfileSetSpy.calls.mostRecent().args[0])
+            // doesn't need to map into UserProfile
+            .toEqual(userProfileResponse);
+        });
+
+      const req = httpMock.expectOne(url);
+      expect(req.request.method).toBe('PATCH');
+      expect(req.request.body).toEqual(userProfileDelta);
+      req.flush(userProfileResponse);
+    });
+
+    it('can test for 403 error', () => {
+      const emsg = 'deliberate 403 error';
+
+      service.patchProfile$(userProfileDelta).subscribe(
+        data => fail('should have failed with the 403 error'),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toEqual(403, 'status');
+          expect(error.error).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.flush(emsg, { status: 403, statusText: 'Forbidden' });
+    });
+
+    it('can test for network error', () => {
+      const emsg = 'simulated network error';
+      const mockError = new ErrorEvent('Network error', {
+        message: emsg,
+      });
+      service.patchProfile$(userProfileDelta).subscribe(
+        data => fail('should have failed with the network error'),
+        (error: HttpErrorResponse) => {
+          expect(error.error.message).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.error(mockError);
+    });
+  });
+
+
+  describe('.putProfile$', () => {
+    const userProfileChanged = {
+      ...userProfileJson,
+      username: 'puttedUser'
+    };
+
+    it('should produce PATCH http request', () => {
+      service.putProfile$(userProfileChanged)
+        .subscribe((res) => {
+          expect(res.username).toBe(userProfileChanged.username);
+          expect(userProfileSetSpy).toHaveBeenCalled();
+          expect(userProfileSetSpy.calls.mostRecent().args[0])
+          // doesn't need to map into UserProfile
+            .toEqual(userProfileChanged);
+        });
+
+      const req = httpMock.expectOne(url);
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toEqual(userProfileChanged);
+      req.flush(userProfileChanged);
+    });
+
+    it('can test for 403 error', () => {
+      const emsg = 'deliberate 403 error';
+
+      service.putProfile$(userProfileChanged).subscribe(
+        data => fail('should have failed with the 403 error'),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toEqual(403, 'status');
+          expect(error.error).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.flush(emsg, { status: 403, statusText: 'Forbidden' });
+    });
+
+    it('can test for network error', () => {
+      const emsg = 'simulated network error';
+      const mockError = new ErrorEvent('Network error', {
+        message: emsg,
+      });
+      service.putProfile$(userProfileChanged).subscribe(
+        data => fail('should have failed with the network error'),
+        (error: HttpErrorResponse) => {
+          expect(error.error.message).toEqual(emsg, 'message');
+        }
+      );
+
+      const req = httpMock.expectOne(url);
+      req.error(mockError);
+    });
+  });
+
+  describe('sessionProfile getter/setter', () => {
+    const testValue = {username: 'ovosch'} as UserProfile;
+    let getterSpy: jasmine.Spy;
+    let setterSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      getterSpy = spyOnProperty(service, 'sessionProfile').and.callThrough();
+      setterSpy = spyOnProperty(service, 'sessionProfile', 'set').and.callThrough();
+    });
+
+    it('Getter should return the session\'s user profile data', () => {
+      userProfileGetSpy.and.returnValue(testValue);
+
+      const testProfile = service.sessionProfile;
+
+      expect(testProfile).toEqual(testValue);
+      expect(getterSpy).toHaveBeenCalled();
+      expect(userProfileGetSpy).toHaveBeenCalled();
+    });
+
+    it('Setter should cast userProfile data to profileBS & session', () => {
+      const testValueMapped = new UserProfile(testValue);
+      const profileNextSpy = spyOn(service.profile, 'next');
+
+      service.sessionProfile = testValue;
+
+      expect(profileNextSpy).toHaveBeenCalled();
+      expect(profileNextSpy.calls.mostRecent().args[0])
+        .toEqual(testValueMapped);
+      expect(setterSpy).toHaveBeenCalled();
+      expect(setterSpy.calls.mostRecent().args[0])
+      // Expected not modified value because then in SessionService it will become string.
+        .toEqual(testValue);
     });
   });
 });
